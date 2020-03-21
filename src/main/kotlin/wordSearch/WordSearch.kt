19622547b295ba4ad2d5dc0fea383b14728b9fe1
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-03-21 16:35:37 +0000

    Include single horizontal word in created grid

--- src/main/kotlin/wordSearch/WordSearch.kt
+++ src/main/kotlin/wordSearch/WordSearch.kt
@@ -1,15 +1,41 @@
 package wordSearch
 
-fun createWordSearch(): List<List<Char>> {
-    val allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXTZ"
+fun createWordSearch(word: String): List<List<Char>> {
+    val letters = "ABCDEFGHIJKLMNOPQRSTUVWXTZ"
 
     fun createRow() = (1..14).map {
-        allowedChars.random()
+        letters.random()
     }
 
-    return (1..14).map { createRow() }
+    val grid = (1..14).map { createRow() }
+
+    fun insertWord(grid: List<List<Char>>, word: String): List<List<Char>> {
+        val randomRowNumber = (grid.indices).random()
+        val row = grid[randomRowNumber]
+
+        fun intRange(wordLength: Int): IntRange {
+            val startInt = (0 until 14 - wordLength).random()
+            val endInt = startInt + wordLength - 1
+            return IntRange(startInt, endInt)
+        }
+
+        val newRow = row.joinToString("").replaceRange(
+            range = intRange(word.length),
+            replacement = word
+        ).toList()
+
+        return grid.mapIndexed { index, existingRow ->
+            if (index == randomRowNumber) {
+                newRow
+            } else {
+                existingRow
+            }
+        }
+    }
+
+    return insertWord(grid, word)
 }
 
 fun main() {
-    createWordSearch().map { println(it) }
+    createWordSearch("").map { println(it) }
 }
--- src/test/kotlin/wordSearch/WordSearchTest.kt
+++ src/test/kotlin/wordSearch/WordSearchTest.kt
@@ -1,16 +1,26 @@
 package wordSearch
 
 import assertk.assertThat
+import assertk.assertions.contains
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 class WordSearchTest {
     @Test
-    fun `should produce 14x14 character grid`() {
-        val grid = createWordSearch()
+    fun `should create 14x14 character grid`() {
+        val grid = createWordSearch("")
 
         assertThat(grid.size).isEqualTo(14)
         assertThat(grid[0].size).isEqualTo(14)
         assertThat(grid.flatten().size).isEqualTo(196)
+    }
+
+    @Test
+    fun `should create grid with a horizontal location word included`() {
+        val grid = createWordSearch("TESTLOCATION")
+
+        val result = grid.flatten().joinToString("")
+
+        assertThat(result).contains("TESTLOCATION")
     }
 }
