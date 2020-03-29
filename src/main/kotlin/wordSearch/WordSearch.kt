4057fb3a5d91ed7db648f81d05892f8fa8b36e74
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-03-29 15:10:04 +0100

    Insert vertical word in created grid and fix flatMap issue

--- src/main/kotlin/wordSearch/WordSearch.kt
+++ src/main/kotlin/wordSearch/WordSearch.kt
@@ -1,6 +1,6 @@
 package wordSearch
 
-fun createWordSearch(words: List<String>): List<List<Char>> {
+fun createWordSearch(words: List<String>, plane: String = "row"): List<List<Char>> {
     val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     fun createRow() = (1..14).map {
@@ -10,8 +10,17 @@ fun createWordSearch(words: List<String>): List<List<Char>> {
     val grid = (1..14).map { createRow() }
 
     fun insertWords(grid: List<List<Char>>, words: List<String>): List<List<Char>> {
+        fun createStartingGrid(): List<List<Char>> {
+            return when (plane) {
+                "vertical" -> grid.transpose()
+                else -> grid
+            }
+        }
+
+        val startingGrid = createStartingGrid()
+
         val wordsAndRowsToHideThemIn =
-            grid.indices
+            startingGrid.indices
                 .shuffled()
                 .take(words.size)
                 .mapIndexed { index, rowNumber -> Pair(rowNumber, words[index]) }
@@ -23,7 +32,7 @@ fun createWordSearch(words: List<String>): List<List<Char>> {
         }
 
         fun createNewRowWithHiddenWord(rowNumber: Int, word: String): List<Char> {
-            return grid[rowNumber]
+            return startingGrid[rowNumber]
                 .joinToString("")
                 .replaceRange(
                     range = rangeInRowToBeReplaced(word.length),
@@ -32,14 +41,17 @@ fun createWordSearch(words: List<String>): List<List<Char>> {
                 .toList()
         }
 
-        return grid.mapIndexed { index, existingRow ->
-            wordsAndRowsToHideThemIn.flatMap {
-                if (index == it.first) {
-                    createNewRowWithHiddenWord(it.first, it.second)
-                } else {
-                    existingRow
-                }
-            }
+        val gridWithHiddenWords = startingGrid.mapIndexed { index, existingRow ->
+             if (wordsAndRowsToHideThemIn.any { it.first == index }) {
+                 createNewRowWithHiddenWord(index, wordsAndRowsToHideThemIn.single { it.first == index }.second)
+             } else {
+                 existingRow
+             }
+        }
+
+        return when (plane) {
+            "vertical" -> gridWithHiddenWords.transpose()
+            else -> gridWithHiddenWords
         }
     }
 
@@ -50,3 +62,27 @@ fun main() {
     createWordSearch(listOf("A", "B", "C", "D", "E", "F", "A", "B", "C", "D", "E", "F")).map { println(it) }
     println()
 }
+
+fun <E> List<List<E>>.transpose(): List<List<E>> {
+    val width = first().size
+
+    return (0 until width).map { col ->
+        (0 until size).map { row -> this[row][col] }
+    }
+}
+
+//fun main() {
+//    val grid = listOf(listOf(1, 2, 3), listOf(4, 5, 6), listOf(7, 8, 9))
+//
+//    println(grid.map { println(it) })
+//    // [1, 2, 3]
+//    // [4, 5, 6]
+//    // [7, 8, 9]
+//
+//    println("----------")
+//
+//    println(grid.transpose().map { println(it) })
+//    // [1, 4, 7]
+//    // [2, 5, 8]
+//    // [3, 6, 9]
+//}
--- src/test/kotlin/wordSearch/WordSearchTest.kt
+++ src/test/kotlin/wordSearch/WordSearchTest.kt
@@ -10,38 +10,67 @@ class WordSearchTest {
     fun `should create 14x14 character grid`() {
         val grid = createWordSearch(listOf(""))
 
+        grid.map { println(it) }
+
         assertThat(grid.size).isEqualTo(14)
         assertThat(grid[0].size).isEqualTo(14)
         assertThat(grid.flatten().size).isEqualTo(196)
     }
 
     @Test
-    fun `should create grid with a horizontal location word included`() {
+    fun `should create grid with a horizontal word included`() {
         val grid = createWordSearch(listOf("TESTLOCATION"))
 
+        grid.map { println(it) }
         val result = grid.flatten().joinToString("")
 
+        assertThat(grid.size).isEqualTo(14)
+        assertThat(grid[0].size).isEqualTo(14)
+        assertThat(grid.flatten().size).isEqualTo(196)
         assertThat(result).contains("TESTLOCATION")
     }
 
     @Test
-    fun `should create grid with 2 horizontal location words included`() {
+    fun `should create grid with 2 horizontal words included`() {
         val grid = createWordSearch(listOf("FIRSTWORD", "SECONDWORD"))
 
+        grid.map { println(it) }
         val result = grid.flatten().joinToString("")
 
+        assertThat(grid.size).isEqualTo(14)
+        assertThat(grid[0].size).isEqualTo(14)
+        assertThat(grid.flatten().size).isEqualTo(196)
         assertThat(result).contains("FIRSTWORD")
         assertThat(result).contains("SECONDWORD")
     }
 
     @Test
-    fun `should create grid with 3 horizontal location words included`() {
+    fun `should create grid with 3 horizontal words included`() {
         val grid = createWordSearch(listOf("FIRSTWORD", "SECONDWORD", "THIRDWORD"))
 
+        grid.map { println(it) }
         val result = grid.flatten().joinToString("")
 
+        assertThat(grid.size).isEqualTo(14)
+        assertThat(grid[0].size).isEqualTo(14)
+        assertThat(grid.flatten().size).isEqualTo(196)
         assertThat(result).contains("FIRSTWORD")
         assertThat(result).contains("SECONDWORD")
         assertThat(result).contains("THIRDWORD")
+    }
+
+    @Test
+    fun `should create grid with a vertical word included`() {
+        val grid = createWordSearch(listOf("VERTICALWORD"), "vertical")
+
+        grid.map { println(it) }
+
+        //transposing again below so we can read word horizontally in test
+        val result = grid.transpose().flatten().joinToString("")
+
+        assertThat(grid.size).isEqualTo(14)
+        assertThat(grid[0].size).isEqualTo(14)
+        assertThat(grid.flatten().size).isEqualTo(196)
+        assertThat(result).contains("VERTICALWORD")
     }
 }
